b475d270f0017903dc76cf04b9589d10ce1f7843
dmitry-pavlov <dimaka@dimaka.com>
2014-03-19 15:53:15 +0400

    added odesk C# test tasks code

---
+++ oDesk/Tests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharpTest
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void TestMaxSum()
+        {
+            MaxSum(new[] {2, -3, 2, 2, -1, 2, -2, 1});
+        }
+
+        [TestMethod]
+        public void TestSearchProducts()
+        {
+            var products = new[] {"boat", "bike", "phone"};
+            var queries = new[] {"b", "bk", "PHo", "bO"};
+            SearchProducts(products, queries);
+        }
+
+        public void SearchProducts(string[] products, string[] queries)
+        {
+            const string notFound = "-1";
+            Array.Sort(products);
+
+            queries
+                .Select(query => products.FirstOrDefault(product => product.StartsWith(query, true, null)))
+                .Select(matchedProduct => matchedProduct ?? notFound)
+                .ToList()
+                .ForEach(Console.WriteLine);
+        }
+
+        public void MaxSum(int[] numbers)
+        {
+            var maxSoFar = numbers[0];
+            var maxEndingHere = numbers[0];
+
+            for (var i = 1; i < numbers.Length; i++)
+            {
+                var number = numbers[i];
+
+                maxEndingHere = maxEndingHere >= 0
+                    ? maxEndingHere + number
+                    : number;
+
+                if (maxEndingHere < maxSoFar) continue;
+
+                maxSoFar = maxEndingHere;
+            }
+
+            Console.WriteLine(maxSoFar);
+        }
+    }
+}
